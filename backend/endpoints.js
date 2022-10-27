5f98bba615a6278a1b9e0936a487d9013d72fe98
Juan Romero <jaromero6@uc.cl>
2022-10-27 11:39:06 -0300

    feat(api): set up express server

---
+++ backend/endpoints.js
@@ -0,0 +1,14 @@
+function createUser(req, res) {
+    res.send('Por implementar');
+}
+
+
+function getUser(req, res) {
+    res.send('Por implementar');
+}
+
+
+module.exports = {
+    createUser,
+    getUser
+};
---
+++ backend/index.js
@@ -0,0 +1,27 @@
+
+// Se importa express para levantar el servidor
+const express = require('express');
+const app = express();
+
+const { getUser, createUser } = require('./endpoints');
+
+// BodyParser permite parsear el body que viene en formato json a un objeto de js
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Se definen los endpoints
+app.get('/user', (req, res) => {
+  getUser(req, res);
+});
+
+app.post('/user',(req, res) => {
+    createUser(req, res);
+} )
+
+
+// Se monta la app en el puerto 8000
+app.listen(8000, () => {
+  console.log('Backend mount at 8000!')
+});
