129b8e7e728114960d74f66074c115c1aff4f6b1
Juan Romero <jaromero6@uc.cl>
2022-10-27 17:22:43 -0300

    fix: consistency between datatype of frontend and backend

--- backend/constants.js
+++ backend/constants.js
@@ -1,9 +1,10 @@
 const maleGender = 'M';
 const femaleGender = 'F';
 
-const womenShoeSize = new Set([22.1,,22.6, 22.9, 23.4, 23.9, 24.1, 24.6, 24.9, 25.4, 25.9]);
-const menShoeSize = new Set([23.3, 23.9, 24.1, 24.6, 24.9, 25.4, 25.9,
-                             26.4, 26.7, 27.2, 27.7, 27.9, 28.4, 28.7]);
+const womenShoeSize = new Set(['22.1','22.6', '22.9', '23.4', '23.9',
+                               '24.1', '24.6', '24.9', '25.4', '25.9']);
+const menShoeSize = new Set(['23.3', '23.9', '24.1', '24.6', '24.9', '25.4', '25.9',
+                             '26.4', '26.7', '27.2', '27.7', '27.9', '28.4', '28.7']);
 
 module.exports = {
     maleGender,
